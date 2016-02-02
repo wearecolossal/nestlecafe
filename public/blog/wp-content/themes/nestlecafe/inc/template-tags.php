c36a8883d8002af9aedb28e8c0b31c3f752791f3
Tarun Krishnan <tarun@tarunkrishnan.com>
2016-02-02 12:40:48 -0500

    updates to the blog

--- public/blog/wp-content/themes/nestlecafe/inc/template-tags.php
+++ public/blog/wp-content/themes/nestlecafe/inc/template-tags.php
@@ -25,14 +25,15 @@ function nestlecafe_posted_on() {
 	);
 
 	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'nestlecafe' ),
+		esc_html_x( '%s', 'post date', 'nestlecafe' ),
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
-	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'nestlecafe' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
+//	$byline = sprintf(
+//		esc_html_x( 'by %s', 'post author', 'nestlecafe' ),
+//		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+//	);
+    $byline = null;
 
 	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
 
