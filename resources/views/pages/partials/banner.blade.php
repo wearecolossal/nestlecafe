ce19daaa7adb407f5a7181ce6f01b58d17c9b64e
Tarun Krishnan <tarun@tarunkrishnan.com>
2016-02-05 14:48:06 -0500

    updated homepage

--- resources/views/pages/partials/banner.blade.php
+++ resources/views/pages/partials/banner.blade.php
@@ -1,6 +1,13 @@
-<div class="banner @yield('banner')" style="background: @yield('hex') url(@yield('background'))">
+<div class="banner @yield('banner')" style="background: url(@yield('background'))">
     @if(URL::current() == URL::to('/'))
         <ul class="slider">
+            <li style="background:#871303 url({{ URL::asset('uploads/slideshow/slider-valentine.jpg') }});">
+                <div class="content">
+                    <h2 class="subhead">This Gift</h2>
+                    <h1 class="heading">Takes <br> The Cake</h1>
+                    <p class="tag" style="color:#fff;">Order Your <br>Special Valentine's <br>Day Cookie Cake Today</p>
+                </div>
+            </li>
             <li style="background:#F1F295 url({{ URL::asset('uploads/slideshow/slider-mocha.jpg') }});">
                 <div class="content">
                     <h2 class="subhead">Chill With Our Favorites</h2>
