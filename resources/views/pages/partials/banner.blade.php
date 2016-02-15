126075b5f32a6e6de14e68dcaa07f8d556b3129c
Tarun Krishnan <tarun@tarunkrishnan.com>
2016-02-15 11:29:43 -0500

    hid frappe slideshow

--- resources/views/pages/partials/banner.blade.php
+++ resources/views/pages/partials/banner.blade.php
@@ -15,13 +15,13 @@
                     {{--<p class="tag" style="color:#fff;">Order Your <br>Special Valentine's <br>Day Cookie Cake Today</p>--}}
                 {{--</div>--}}
             {{--</li>--}}
-            <li style="background:#F1F295 url({{ URL::asset('uploads/slideshow/slider-mocha.jpg') }});">
-                <div class="content">
-                    <h2 class="subhead">Chill With Our Favorites</h2>
-                    <h1 class="heading">Mocha <br> Frappes</h1>
-                    <p class="tag">Toll House&reg; Cookie <br> Turtle <br> Nestl&eacute; Crunch <br> Butterfinger</p>
-                </div>
-            </li>
+            {{--<li style="background:#F1F295 url({{ URL::asset('uploads/slideshow/slider-mocha.jpg') }});">--}}
+                {{--<div class="content">--}}
+                    {{--<h2 class="subhead">Chill With Our Favorites</h2>--}}
+                    {{--<h1 class="heading">Mocha <br> Frappes</h1>--}}
+                    {{--<p class="tag">Toll House&reg; Cookie <br> Turtle <br> Nestl&eacute; Crunch <br> Butterfinger</p>--}}
+                {{--</div>--}}
+            {{--</li>--}}
         </ul>
     @else
         <div class="banner-wrapper">
