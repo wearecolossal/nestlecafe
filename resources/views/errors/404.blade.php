5ff375e4147f90ba4b8e6d5ec4dfca231dd84c08
Tarun Krishnan <tarun@tarunkrishnan.com>
2016-02-15 13:48:39 -0500

    update 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -19,10 +19,10 @@
             <div class="content">
                 <h1 style="margin-top:0;padding-top:0;margin-bottom:10px;">The page you’re looking for can not be found, or an error has occurred with the site.</h1>
                 <p style="font-size:1em;width:80%;margin:0 auto;">
-                    If you feel you’re reached this page in error, please fill out our contact form with details of your experience. Otherwise, please continue your browsing experience below.
+                    If you feel you’re reached this page in error, please fill out our <a href="{{ URL::to('contact') }}" style="color:inherit; text-decoration: underline">contact form</a> with details of your experience. Otherwise, please continue your browsing experience below.
                     <br><br>
                 </p>
-                <a href="{{ URL::to('contact') }}" class="btn wired">Contact Us</a>
+                <a href="{{ URL::to('/') }}" class="btn wired">Continue Browsing</a>
             </div>
         </div>
         <div class="clearfix"></div>
