5218ff4cdbf1064dd3a316c7bf50ee56ec49b585
Tarun Krishnan <tarun@tarunkrishnan.com>
2016-02-17 14:54:57 -0500

    updated admin

--- resources/views/admin/blog.blade.php
+++ resources/views/admin/blog.blade.php
@@ -6,7 +6,7 @@
 
     <div class="col-md-6 col-md-offset-3">
         <div class="panel panel-default text-center">
-            <div class="panel-body" style="background:url({{ URL::asset('uploads/page_banners/banner-cookie-talk.jpg') }}) bottom center; background-size:cover;color:#fff;text-shadow:0px 5px 4px rgba(0, 0, 0, .9)">
+            <div class="panel-body">
                 <h3>Log Into Your Blog</h3>
                 <p class="lead">Your login credentials are the same as your admin login credentials</p>
             </div>
