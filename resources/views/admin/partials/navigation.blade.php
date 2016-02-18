4dc0ba4a78302fe03157dac93060967e60bd93c1
Tarun Krishnan <tarun@tarunkrishnan.com>
2016-02-18 16:06:49 -0500

    updated homepage

--- resources/views/admin/partials/navigation.blade.php
+++ resources/views/admin/partials/navigation.blade.php
@@ -1,13 +1,13 @@
 <div class="collapse navbar-collapse navbar-ex1-collapse">
     <ul class="nav navbar-nav side-nav">
         <li class="{{ isActive('admin/slides') }}">
-            <a href="{{ URL::to('admin/slides') }}"><i class="fa fa-fw fa-flag"></i> Promo Banners</a>
+            <a href="{{ URL::to('admin/slides') }}"><i class="fa fa-fw fa-flag"></i> Homepage Banners</a>
+        </li>
+        <li class="{{ isActive('admin/callouts') }}">
+            <a href="{{ URL::to('admin/callouts') }}"><i class="fa fa-fw fa-birthday-cake"></i> Homepage Callouts</a>
         </li>
         <li class="{{ isActive('admin/cafes') }}">
             <a href="{{ URL::to('admin/cafes') }}"><i class="fa fa-fw fa-coffee"></i> Cafe Locations</a>
-        </li>
-        <li class="{{ isActive('admin/callouts') }}">
-            <a href="{{ URL::to('admin/callouts') }}"><i class="fa fa-fw fa-birthday-cake"></i> Home Page Callouts</a>
         </li>
         <li class="{{ isActive('admin/menu') }}">
             <a data-url="{{ URL::to('admin/menu') }}" style="opacity:.2"><i class="fa fa-fw fa-cutlery"></i> Menu <small>pending</small></a>
