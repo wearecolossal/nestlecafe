c84f78fd5dec84fdd2c8b15670c2563b75023ad8
Bryan <bryan@colossal.net>
2016-02-24 17:29:25 -0500

    Careers page button spacing

--- resources/views/pages/careers.blade.php
+++ resources/views/pages/careers.blade.php
@@ -26,6 +26,7 @@
                 <h1 style="margin-top:0;padding-top:0;margin-bottom:10px;">Download Our Employment Application</h1>
                 <p style="font-size:1em;width:80%;margin:0 auto;">
                     We're always looking for potential new employees. Download and fill out our employment application below. Then visit your nearest Nestl&eacute;&reg; Toll House&reg; Caf&eacute; by Chip&reg; location for inquiries.
+                    <br/><br/>
                 </p>
                 <a href="{{ URL::to('library/pdf/employment-application.pdf') }}" target="_blank" class="btn wired">Download</a>
             </div>
